8436241a53cefd10c00bc8a907da55bccf1d94a5
Steven <vosteven96@gmail.com>
2024-12-07 23:57:45 -0600

    added api gateway

--- website/index.js
+++ website/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://yrjpu545z3vvl5l6k7sov4ftju0fmejl.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://5qph39anq3.execute-api.us-east-1.amazonaws.com/dev");
     let data = await response.json();
     counter.innerHTML = `${data} Views`;
 }
